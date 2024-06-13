7e4465b585732f808c185108b23dff0213953ff7
krupangoudsb <67727357+krupan-goud@users.noreply.github.com>
2024-06-13 15:43:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ require('./utils/mongoose.config')
 
 app.use("/portFolio", router);
 
+app.get('/', (req, res) => {
+    res.json('Hello World')
+})
+
 app.listen(process.env.PORT, () => {
     console.info(`Server running at http://${process.env.IP_ADDRESS}:${process.env.PORT}`)
 })
